908e3a9d5992a0b84926209a9efa78255951ca2b
ppsinghgit <parminderpalsingh@live.com>
2022-07-19 12:14:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# count-pps
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hwhi7z-4qqhy2)
---
+++ src/app/icon-sample-1/icon-sample-1.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-sample-1',
+  styleUrls: ['./icon-sample-1.component.scss'],
+  templateUrl: './icon-sample-1.component.html',
+})
+export class IconSample1Component {
+  count: number = 0;
+
+  ok() {
+    this.count++;
+  }
+}
